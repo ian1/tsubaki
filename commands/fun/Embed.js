38b40a91a91f4fae66c87e0eb9ac4b7bb0a5b969
David <me@pantherman594.com>
2017-08-17 13:21:25 -0400

    Add color embeds

--- commands/fun/Embed.js
+++ commands/fun/Embed.js
@@ -11,9 +11,20 @@ function Embed() {
 }
 
 method.execute = function (message, args, bot, db) {
+  let msg = '';
+  let color = Tsubaki.color.gray;
+  if (args.length >= 2 && args[0].length == 6) {
+    color = '0x' + args[0];
+    msg = args.slice(1).join(' ');
+  } else if (args.length >= 1) {
+    msg = args.join(' ')
+  } else {
+    message.channel.send({ embed: Tsubaki.Style.warn("Please tell me what to say!") });
+  }
+  
   let embed = new Discord.RichEmbed()
-    .setDescription(args.join(" "))
-    .setColor(Tsubaki.color.green);
+    .setDescription(msg)
+    .setColor(color);
   message.channel.send({ embed: embed });
 }
 
