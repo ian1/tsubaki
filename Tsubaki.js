707c597fb14642ad6ff928806f3647906cbff752
Unknown <iandomme@gmail.com>
2017-08-19 14:15:58 -0500

    mhm

--- Tsubaki.js
+++ Tsubaki.js
@@ -228,7 +228,7 @@ bot.on('guildMemberAdd', (member) => {
   let guild = member.guild;
   if (guild.id == discordBotGuild) return;
 
-  if (guild.id === tsubakiPalaceGuild) {
+  if ("337747052878626816" === tsubakiPalaceGuild) {
     member.addRole(member.guild.roles.find("name", "Member"));
   }
 
