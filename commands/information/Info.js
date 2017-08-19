a00d7514617b0a94f00547da84644e584ba54d14
David Shen <me@pantherman594.com>
2017-08-19 17:54:26 -0400

    Add github url to info

--- commands/information/Info.js
+++ commands/information/Info.js
@@ -13,7 +13,8 @@ class Info extends Command {
       .setDescription(Tsubaki.Style.bold('Information About ' + Tsubaki.name) + '\n\n     '
         + Tsubaki.name + ' is a ' + Tsubaki.Style.bold('Discord.js') + ' bot. She can be used for moderation, '
         + 'administration, utility, and just for fun. She has a wide range of commands everyone will enjoy, '
-        + 'and she is constantly being updated and added to.' + '\n')
+        + 'and she is constantly being updated and added to.' + '\n'
+        + 'Github Repository: https://github.com/ian1/tsubaki' + '\n')
       .setFooter('Created by ' + Tsubaki.author)
       .setColor(Tsubaki.color.white);
     message.channel.send({ embed: embed });
